430dfb5e8882d212b427ef9ec3d7d8df7466f5e3
Dev-Eliot <AriasEliotDev@gmail.com>
2023-10-03 14:32:57 -0500

    feat: header finished

--- template/footer.php
+++ template/footer.php
@@ -1,7 +1,8 @@
-
-
-
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+	<footer class="footer">
+		<div class="logo"></div>
+		<div class="menu"></div>
+		<div class="contactos"></div>
+		<div class="redes"></div>
+	</footer>
 </body>
 </html>
