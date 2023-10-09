eafa8f540450a8ee90cc5516d3849f44eca05855
Dev-Eliot <AriasEliotDev@gmail.com>
2023-10-09 13:28:21 -0500

    feat: vision & mision

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php include_once $_SERVER['DOCUMENT_ROOT'] . "/Boned/template/header.php";?>   
 
 <?php include_once $_SERVER['DOCUMENT_ROOT'] . "/Boned/Content/vision.php";?>
-<?php include_once $_SERVER['DOCUMENT_ROOT'] . "/Boned/template/footer.php";?>
+<?php //include_once $_SERVER['DOCUMENT_ROOT'] . "/Boned/template/footer.php";?>
