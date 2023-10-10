c510940fe798c1569da011c66f088a54b171b610
Dev-Eliot <ariaseliotdev@gmail.com>
2023-10-10 17:00:19 -0500

    fix: footer responsive

--- Content/vision.php
+++ Content/vision.php
@@ -22,3 +22,99 @@
         </div>
     </div>
 </section>
+<!--section class="nosotros">
+    <div class="nosotros__vision">
+        <div class="nosotros__header">
+            <img class="nosotros__img" src="Images/vision.png" alt="">
+            <h2 class="nosotros__header--titulo">VISIÓN</h2>
+        </div>
+        <div class="nosotros__info">
+            <p>
+                Ser los mejores socios comerciales de nuestros clientes, proveedores y colaboradores, generando confianza y respeto de familia.
+            </p>
+        </div>
+    </div>
+    <div class="nosotros__mision">
+        <div class="nosotros__header">
+            <img class="nosotros__img" src="Images/mision.png" alt="">
+            <h2 class="nosotros__header--titulo">MISIÓN</h2>
+        </div>
+        <div class="nosotros__info">
+            <p>
+                Brindar el mejor servicio de distribución llevando alternativas terapéuticas de calidad y accesibles para su consumos en el sector farmacéutico.
+            </p>
+        </div>
+    </div>
+</section>
+<section class="nosotros">
+    <div class="nosotros__vision">
+        <div class="nosotros__header">
+            <img class="nosotros__img" src="Images/vision.png" alt="">
+            <h2 class="nosotros__header--titulo">VISIÓN</h2>
+        </div>
+        <div class="nosotros__info">
+            <p>
+                Ser los mejores socios comerciales de nuestros clientes, proveedores y colaboradores, generando confianza y respeto de familia.
+            </p>
+        </div>
+    </div>
+    <div class="nosotros__mision">
+        <div class="nosotros__header">
+            <img class="nosotros__img" src="Images/mision.png" alt="">
+            <h2 class="nosotros__header--titulo">MISIÓN</h2>
+        </div>
+        <div class="nosotros__info">
+            <p>
+                Brindar el mejor servicio de distribución llevando alternativas terapéuticas de calidad y accesibles para su consumos en el sector farmacéutico.
+            </p>
+        </div>
+    </div>
+</section>
+<section class="nosotros">
+    <div class="nosotros__vision">
+        <div class="nosotros__header">
+            <img class="nosotros__img" src="Images/vision.png" alt="">
+            <h2 class="nosotros__header--titulo">VISIÓN</h2>
+        </div>
+        <div class="nosotros__info">
+            <p>
+                Ser los mejores socios comerciales de nuestros clientes, proveedores y colaboradores, generando confianza y respeto de familia.
+            </p>
+        </div>
+    </div>
+    <div class="nosotros__mision">
+        <div class="nosotros__header">
+            <img class="nosotros__img" src="Images/mision.png" alt="">
+            <h2 class="nosotros__header--titulo">MISIÓN</h2>
+        </div>
+        <div class="nosotros__info">
+            <p>
+                Brindar el mejor servicio de distribución llevando alternativas terapéuticas de calidad y accesibles para su consumos en el sector farmacéutico.
+            </p>
+        </div>
+    </div>
+</section>
+<section class="nosotros">
+    <div class="nosotros__vision">
+        <div class="nosotros__header">
+            <img class="nosotros__img" src="Images/vision.png" alt="">
+            <h2 class="nosotros__header--titulo">VISIÓN</h2>
+        </div>
+        <div class="nosotros__info">
+            <p>
+                Ser los mejores socios comerciales de nuestros clientes, proveedores y colaboradores, generando confianza y respeto de familia.
+            </p>
+        </div>
+    </div>
+    <div class="nosotros__mision">
+        <div class="nosotros__header">
+            <img class="nosotros__img" src="Images/mision.png" alt="">
+            <h2 class="nosotros__header--titulo">MISIÓN</h2>
+        </div>
+        <div class="nosotros__info">
+            <p>
+                Brindar el mejor servicio de distribución llevando alternativas terapéuticas de calidad y accesibles para su consumos en el sector farmacéutico.
+            </p>
+        </div>
+    </div>
+</section-->
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<?php include_once $_SERVER['DOCUMENT_ROOT'] . "/Boned/template/header.php";?>   
+<?php include_once $_SERVER['DOCUMENT_ROOT'] . "/BonedLandingPage/template/header.php";?>   
 
-<?php include_once $_SERVER['DOCUMENT_ROOT'] . "/Boned/Content/vision.php";?>
-<?php //include_once $_SERVER['DOCUMENT_ROOT'] . "/Boned/template/footer.php";?>
+<?php include_once $_SERVER['DOCUMENT_ROOT'] . "/BonedLandingPage/Content/vision.php";?>
+
+<?php include_once $_SERVER['DOCUMENT_ROOT'] . "/BonedLandingPage/template/footer.php";?>
